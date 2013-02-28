0f2d3994ddac43c817e7dd30a063498361634351
Jonas Kastberg <jihgfee@gmail.com>
2013-02-28 21:25:14 +0100

    Bitbucket format fix

--- MiningNobel.java
+++ MiningNobel.java
@@ -6,15 +6,15 @@ public class MiningNobel
 	//This method finds the amount of 4-tuples with ~N^4 runtime - N being the length of a
 	public static int exhaustiveCount(double[] a) 
 	{
-        int N = a.length;
-        
+		int N = a.length;
+		
 		int count = 0;
-        
+		
 		for (int i = 0; i < N; i++) 
 		{
-            for (int j = 0; j < N; j++) 
+			for (int j = 0; j < N; j++) 
 			{
-                for (int k = 0; k < N; k++) 
+				for (int k = 0; k < N; k++) 
 				{
 					for(int l = 0; l < N; l++)
 					{
@@ -27,38 +27,38 @@ public class MiningNobel
             }
         }
 		
-        return count;
+		return count;
     }
 	
 	//This method finds the amount of 4-tuples with ~N^3*log(n) runtime - N being the length of a
 	public static int fastCount(double[] a) 
 	{
-        int N = a.length;
+		int N = a.length;
         
 		int count = 0;
-        Arrays.sort(a);
+		Arrays.sort(a);
 		
 		for (int i = 0; i < N; i++) 
 		{
-            for (int j = 0; j < N; j++) 
+			for (int j = 0; j < N; j++) 
 			{
-                for (int k = 0; k < N; k++) 
+				for (int k = 0; k < N; k++) 
 				{
 					int l = Arrays.binarySearch(a, -(a[i] + a[j] + a[k]));		//We iterate through the first 3 numbers, and then search for the last one
 					
 					if (l > -1) count++;										//We make sure that the returned index is an actual array index (more than -1)
-                }
-            }
+				}
+			}
         }
 		
-        return count;
+		return count;
     } 
 	
 	//This method finds the amount of 4-tuples with ~N^2*log(n) runtime - N being the length of a
 	public static int cuteCount(double[] a)
 	{
 		int N = a.length;
-        
+		
 		double[] b = new double[a.length*a.length];					//We make a new array containing all the summed values of the array a
 		HashMap<Double, Integer> map = new HashMap<>();
 		
@@ -81,12 +81,12 @@ public class MiningNobel
 		}
 	
 		int count = 0;
-        Arrays.sort(a);
+		Arrays.sort(a);
 		Arrays.sort(b);
 		
 		for (int i = 0; i < N; i++) 
 		{
-            for (int j = 0; j < N; j++) 
+			for (int j = 0; j < N; j++) 
 			{
 				double numberToFind = a[i] + a[j] != 0 ? -(a[i] + a[j]) : 0;	//If the number we want to search for is not 0 we reverse it, else we assign 0 to the variable
 				
@@ -96,10 +96,10 @@ public class MiningNobel
 				{
 					count += map.get(numberToFind);								//We check our hashmap to see how many duplicates of the found number we have
 				}
-            }
-        }
+			}
+		}
 			
-        return count;
+		return count;
 	}
 	
     public static void main(String[] args)  
@@ -112,9 +112,8 @@ public class MiningNobel
 			doubleArray[i] = Double.parseDouble(strings[i].split(",")[1].trim());	//We iterate through our array and parse the double of each line to our double array
 		
 		Stopwatch timer = new Stopwatch();
-        int count = cuteCount(doubleArray);											//We find the count of 4-tuples. (exhaustiveCount, fastCount, cuteCount may be applied)
-        StdOut.println("elapsed time = " + timer.elapsedTime());
-        StdOut.println(count);
-		
+		int count = cuteCount(doubleArray);											//We find the count of 4-tuples. (exhaustiveCount, fastCount, cuteCount may be applied)
+		StdOut.println("elapsed time = " + timer.elapsedTime());
+		StdOut.println(count);
     }
 }
